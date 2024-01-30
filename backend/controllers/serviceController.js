c62d8a58d2d27d02b332fcb3ccfbe9a63d0d6526
gabrielestefono <gabrielestefono@hotmail.com>
2024-01-30 06:28:35 -0300

    Criando serviço de banco de dados

--- backend/controllers/serviceController.js
+++ backend/controllers/serviceController.js
@@ -1,5 +1,22 @@
 const { Service: ServiceModel } = require('../models/service');
 
-const serviceController = {}
+const serviceController = {
+	create: async(req, res) => {
+		try {
+			const service = {
+				name: req.body.name,
+				description: req.body.description,
+				prince: req.body.price,
+				image: req.body.image,
+			};
+
+			const response = await ServiceModel.create(service);
+
+			res.state(201).json({response, msg: "Serviço criado com sucesso!"});
+		} catch (error) {
+			console.log(error)
+		}
+	}
+}
 
 module.exports = serviceController;
